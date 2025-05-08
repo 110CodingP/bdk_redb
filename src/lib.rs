6e0f5c311669b22d533412b3c6184982ba03c034
codingp110 <codingp110@gmail.com>
2025-05-08 19:38:19 +0530

    feat: implement read_network and persist_network
    For the NETWORK table went with wallet_name as the key to allow for
    one db storing multiple wallets. Also took wallet_name as &str since
    would need to pass it on to various persist functions(considering
    adding this as a field in Store), while for network went with String as
    it seemed easier to convert bitcoin::Network to.
    
    The redb::ReadTransaction::open_table function throws error if table
    hasn't been created already so planning to introduce an init_tables
    function in another commit. Adopted bdk_sqlx style and chose to panic
    rather than return an error if network is parsed incorrectly. In case of
    network not being persisted already chose to return error instead since
    this might happen easily if user is not careful IMO.
    
    Also chose to not differentiate between read_txn and write_txn in naming
    following bdk_sqlx, rusqlite_impl etc. could change that if necessary

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,23 @@
-use redb::Database;
-use std::path::Path;
+use bdk_wallet::ChangeSet;
+use bdk_wallet::bitcoin::Network;
+use redb::{Database, ReadTransaction, TableDefinition, WriteTransaction};
+use std::{path::Path, str::FromStr};
+
+const NETWORK: TableDefinition<&str, String> = TableDefinition::new("network");
 
 #[derive(Debug, thiserror::Error)]
 pub enum BdkRedbError {
     #[error(transparent)]
     DBError(#[from] redb::DatabaseError),
+
+    #[error(transparent)]
+    TableError(#[from] redb::TableError),
+
+    #[error("network yet to be persisted")]
+    NetworkPersistError,
+
+    #[error(transparent)]
+    RedbStorageError(#[from] redb::StorageError),
 }
 
 pub struct Store {
@@ -18,5 +31,24 @@ impl Store {
     {
         let db = Database::create(file_path)?;
         Ok(Store { db })
+    }
+
+    pub fn persist_network(txn: &WriteTransaction, network: &Network, wallet_name: &str) -> Result<(), BdkRedbError> {
+        let mut table = txn.open_table(NETWORK)?;
+        let _ = table.insert(wallet_name, network.to_string());
+        Ok(())
+    }
+
+    pub fn read_network(
+        txn: &ReadTransaction,
+        changeset: &mut ChangeSet,
+        wallet_name: &str,
+    ) -> Result<(), BdkRedbError> {
+        let table = txn.open_table(NETWORK)?;
+        changeset.network = match table.get(wallet_name)? {
+            Some(network) => Some(Network::from_str(&network.value()).expect("parse network")),
+            None => return Err(BdkRedbError::NetworkPersistError),
+        };
+        Ok(())
     }
 }
