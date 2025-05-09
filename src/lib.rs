b496036b82fb6b24d06ccc9b613aca25bf9659d2
codingp110 <codingp110@gmail.com>
2025-05-09 11:00:50 +0530

    refactor:use redb Error wrapper as error variant
    Otherwise BdkRedbError just explodes with redb error variants!

--- src/lib.rs
+++ src/lib.rs
@@ -8,22 +8,10 @@ const NETWORK: TableDefinition<&str, String> = TableDefinition::new("network");
 #[derive(Debug, thiserror::Error)]
 pub enum BdkRedbError {
     #[error(transparent)]
-    DBError(#[from] redb::DatabaseError),
-
-    #[error(transparent)]
-    TableError(#[from] redb::TableError),
+    RedbError(#[from] redb::Error),
 
     #[error("network yet to be persisted")]
     NetworkPersistError,
-
-    #[error(transparent)]
-    RedbStorageError(#[from] redb::StorageError),
-
-    #[error(transparent)]
-    RedbCommitError(#[from] redb::CommitError),
-
-    #[error(transparent)]
-    RedbTransactionError(#[from] redb::TransactionError),
 }
 
 pub struct Store {
@@ -36,7 +24,7 @@ impl Store {
     where
         P: AsRef<Path>,
     {
-        let db = Database::create(file_path)?;
+        let db = Database::create(file_path).map_err(redb::Error::from)?;
         Ok(Store { db, wallet_name })
     }
 
@@ -45,15 +33,15 @@ impl Store {
         db_tx: &WriteTransaction,
         network: &Network,
     ) -> Result<(), BdkRedbError> {
-        let mut table = db_tx.open_table(NETWORK)?;
+        let mut table = db_tx.open_table(NETWORK).map_err(redb::Error::from)?;
         let _ = table.insert(&*self.wallet_name, network.to_string());
         Ok(())
     }
 
     pub fn create_tables(&mut self) -> Result<(), BdkRedbError> {
-        let db_tx = self.db.begin_write()?;
+        let db_tx = self.db.begin_write().map_err(redb::Error::from)?;
         let _ = db_tx.open_table(NETWORK);
-        db_tx.commit()?;
+        db_tx.commit().map_err(redb::Error::from)?;
         Ok(())
     }
 
@@ -62,8 +50,8 @@ impl Store {
         db_tx: &ReadTransaction,
         changeset: &mut ChangeSet,
     ) -> Result<(), BdkRedbError> {
-        let table = db_tx.open_table(NETWORK)?;
-        changeset.network = match table.get(&*self.wallet_name)? {
+        let table = db_tx.open_table(NETWORK).map_err(redb::Error::from)?;
+        changeset.network = match table.get(&*self.wallet_name).map_err(redb::Error::from)? {
             Some(network) => Some(Network::from_str(&network.value()).expect("parse network")),
             None => return Err(BdkRedbError::NetworkPersistError),
         };
@@ -84,16 +72,12 @@ mod test {
 
     fn test_network_persistance(store: &Store) {
         let db_tx = store.db.begin_write().unwrap();
-        store
-            .persist_network(&db_tx, &Network::Bitcoin)
-            .unwrap();
+        store.persist_network(&db_tx, &Network::Bitcoin).unwrap();
         db_tx.commit().unwrap();
 
         let db_tx = store.db.begin_read().unwrap();
         let mut changeset = ChangeSet::default();
-        store
-            .read_network(&db_tx, &mut changeset)
-            .unwrap();
+        store.read_network(&db_tx, &mut changeset).unwrap();
 
         assert_eq!(changeset.network, Some(Network::Bitcoin));
     }
