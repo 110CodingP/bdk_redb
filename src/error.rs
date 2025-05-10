5278930b259877e0440e6f04416751a7acddcc39
codingp110 <codingp110@gmail.com>
2025-05-10 23:47:24 +0530

    refactor!: separate and rename persist errors

---
+++ src/error.rs
@@ -0,0 +1,9 @@
+#[derive(Debug, thiserror::Error)]
+pub enum MissingError {
+
+    #[error("network yet to be persisted")]
+    NetworkPersistError,
+
+    #[error("descriptor yet to be persisted")]
+    DescPersistError { num_descs: u64 }, // upper bound on the number of descriptors found in the db
+}
