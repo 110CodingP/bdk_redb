6e4b6b783ebc476c02741bae43c8169a428b287d
codingp110 <codingp110@gmail.com>
2025-06-29 13:56:43 +0530

    refactor!: remove extraneous error variant

--- src/error.rs
+++ src/error.rs
@@ -3,17 +3,4 @@
 pub enum BdkRedbError {
     #[error(transparent)]
     RedbError(#[from] redb::Error),
-
-    #[error(transparent)]
-    DataMissingError(#[from] MissingError),
-}
-
-// Enum that groups all errors which deal with a structure not being persisted.
-#[derive(Debug, thiserror::Error)]
-pub enum MissingError {
-    #[error("network yet to be persisted")]
-    NetworkPersistError,
-
-    #[error("descriptor yet to be persisted")]
-    DescPersistError { num_descs: u64 }, // upper bound on the number of descriptors found in the db
 }
