594a62d86c7cdb044a5ebef4ff23ad995b919273
codingp110 <codingp110@gmail.com>
2025-06-29 15:10:27 +0530

    refactor: improve error message on RedbError

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,6 @@
 // Superset of all errors that can occur.
 #[derive(Debug, thiserror::Error)]
 pub enum BdkRedbError {
-    #[error(transparent)]
+    #[error("Database error: {0}")]
     RedbError(#[from] redb::Error),
 }
